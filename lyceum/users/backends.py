ec811f9bb80817f4bf49535b93b21b5886af7a3e
Aramarchuk <aramarchuk42@gmail.com>
2023-11-17 00:55:37 +0500

    AAAAAAAAA

--- lyceum/users/backends.py
+++ lyceum/users/backends.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 
 from users.models import User
@@ -7,20 +8,15 @@ __all__ = "EmailAuthBackend"
 
 class EmailAuthBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
-        if username is None or password is None:
-            return None
+        user_model = get_user_model()
         try:
-            User.USERNAME_FIELD = "email"
-            user = User.objects._default_manager.get_by_natural_key(username)
-        except User.DoesNotExist:
-            # Run the default password hasher once to reduce the timing
-            # difference between an existing and a nonexistent user (#20760).
-            User().set_password(password)
+            user = user_model.objects.get(email=username)
+        except user_model.DoesNotExist:
+            return None
         else:
-            if user.check_password(password) and self.user_can_authenticate(
-                user,
-            ):
+            if user.check_password(password):
                 return user
+        return None
 
     def get_user(self, user_id):
         try:
